d1c9ab4b8f3353d00982ac6b4b9c0359afd986d1
Szilard Nemeth <nsziszy@gmail.com>
2019-11-02 21:07:06 +0100

    Add initial version of unarchive.js

---
+++ unarchive.js
@@ -0,0 +1,50 @@
+function openArchivedMenu() {
+	$("span:contains('Show Menu')").trigger('click')
+	$('.js-open-more').trigger('click')
+	$('.js-open-archive').trigger('click')	
+}
+
+function loadArchivedCards() {
+	var i = 0;
+
+	var clickShowMore = function(i) {
+	  console.log("clicking More items. Iteration " + i)
+	  $('.show-more').trigger("click");
+	};
+
+	var recursiveClickShowMore = function() {
+	  clickShowMore(i);
+	  i += 1;
+	  setTimeout(function() {
+	    if (i < 100 && $('.show-more').is(':visible')) {
+	      recursiveClickShowMore();
+	    }
+	  }, 1000);
+	}
+
+	recursiveClickShowMore();
+}
+
+function sendArchivedCardsToBoardInt() {
+	$('.archived-list-card').find('a').filter('.js-reopen').each(function(index) {        
+	        (function(that, i) { 
+	            var t = setTimeout(function() { 
+	            	console.log("Clicking item with index: " + i);
+	            	console.log("Clicking item: " + that)
+	                //$(that).click()
+	            }, 500 * i);
+	        })(this, index);
+	    });
+}
+
+
+function sendAllArchivedCardsToBoard() {
+	//step 1: open archived cards view
+	openArchivedMenu();
+
+	//step 2: load all archived cards
+	loadArchivedCards();
+
+	//step 3: trigger clicks on all cards
+	sendArchivedCardsToBoardInt();
+}
