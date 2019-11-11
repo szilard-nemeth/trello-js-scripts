8fc49ab76f9776a68eb74574d24d1effa87f775d
Szilard Nemeth <szilard.nemeth88@gmail.com>
2019-11-11 09:38:15 +0100

    export-parser.js: Put elements to a map that holds list names as keys and cards as values

--- export-parser.js
+++ export-parser.js
@@ -26,23 +26,36 @@ function loadChecklists() {
 	return mapOfChecklists
 }
 
+function loadLists() {
+	var json = $('body pre').html()
+	var jsonObj = JSON.parse(json)
+	var lists = jsonObj['lists']
+
+	var mapOfLists = lists.reduce(function(map, obj) {
+    	map.set(obj.id, obj);
+    	return map;
+	}, new Map());
+	return mapOfLists
+}
+
 function findCardsByName(name) {
 	var cards = loadCards()
 	var filteredCards = $.grep(cards, function(obj){return obj.name === name;});
 	return filteredCards
 }
 
-function convertToCardObjects(cards, checklists) {
+function convertToCardObjects(cards, checklists, lists) {
 	if (!Array.isArray(cards)) {
 		cards = new Array(cards)
 	}
 	console.log("Received cards: ", cards)
-	return cards.map(c => {
+	cards = cards.map(c => {
 		console.log("Converting card: ", c)
 		var cardObject = new Object();
 		cardObject.title = c['name']
 		cardObject.description = c['desc'] //TODO format as html
 		cardObject.labels = c['labels'].map(c => c.name)
+		cardObject.listId = c['idList']
 
 		var checklistIds = c['idChecklists']
 		if (checklistIds != null && checklistIds != undefined) {
@@ -68,24 +81,58 @@ function convertToCardObjects(cards, checklists) {
 
 		// cardObject.activityHistory = historyObjects
 		// cardObject.comments = commentObjects
+		console.log("Converted card: ", cardObject)
 		return cardObject
 	})
+
+	var listIdSet = new Set()
+	cards.forEach(c => listIdSet.add(c.listId))
+	console.log("Found list ids: ", listIdSet)
+	console.log("Found distinct list ids: " + listIdSet.size)
+
+	var cardResultMap = cards.reduce(function(map, obj) {
+		// console.log("***processing obj: ", obj)
+		// console.log("***processing obj: ", obj.listId)
+		//TODO what happens if two (or more) lists are having the same name
+		// if (!map.has(obj.listId)) {
+		// 	map.set(obj.listId, [])
+		// }
+  //   	map.get(obj.listId).push(obj);
+
+  		var listName = lists.get(obj.listId)['name']
+  		if (!map.has(listName)) {
+			map.set(listName, [])
+		}
+    	map.get(listName).push(obj);
+    	return map;
+	}, new Map());
+
+	console.log("Found distinct list ids for final card result map: ", cardResultMap.size)
+
+	if (cardResultMap.size != listIdSet.size) {
+		throw "Number of found lists for all cards does not match number of lists in final result map! This is most likely due to duplicate list names"
+	}
+
+	return cardResultMap
 }
 
 
 importJquery()
+
 //Run these commands to fetch a particular card
 var checklists = loadChecklists()
+var lists = loadLists()
 var cards = findCardsByName('Drive: kepeket megosztani: Apa/Viola')
 // var cards = findCardsByName('testcard')
-var cardObj = convertToCardObjects(cards[0], checklists)
+var cardObj = convertToCardObjects(cards[0], checklists, lists)
 cardObj
 cardObj[0].checklists[0].items
 
 //Run these commands to convert all cards
 var checklists = loadChecklists()
+var lists = loadLists()
 var cards = loadCards()
-var cardObjs = convertToCardObjects(cards, checklists)
+var cardObjs = convertToCardObjects(cards, checklists, lists)
 cardObjs
 
 
